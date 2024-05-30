a634349b8af3ed3ef01f958fc54f028cbf756406
lemon-mint <ice.lemons.mint@gmail.com>
2024-05-30 17:31:19 +0900

    coord/provider

--- coord.go
+++ coord.go
@@ -1,1 +1,35 @@
 package coord
+
+import (
+	"sort"
+	"sync"
+
+	"github.com/lemon-mint/coord/provider"
+)
+
+var (
+	providersMu sync.RWMutex
+	providers   = make(map[string]provider.Provider)
+)
+
+// Providers returns the names of the registered providers.
+func Providers() []string {
+	providersMu.RLock()
+	defer providersMu.RUnlock()
+	list := make([]string, 0, len(providers))
+	for name := range providers {
+		list = append(list, name)
+	}
+	sort.Strings(list)
+	return list
+}
+
+// RegisterProvider registers a provider.
+func RegisterProvider(name string, p provider.Provider) {
+	providersMu.Lock()
+	defer providersMu.Unlock()
+	if p == nil {
+		panic("coord: Register provider is nil")
+	}
+	providers[name] = p
+}
---
+++ provider/provider.go
@@ -0,0 +1,4 @@
+package provider
+
+type Provider interface {
+}
